fedcb069d4cbe3a30f2a074d4d794a0c5a1f9ff3
asingh <asingh@febdc8c4-1326-0410-abd6-27cb15717650>
2007-10-09 02:51:16 +0000

    Tagging the 0.9.4 prerelease.

---
+++ tags/macfuse-0.9.4/core/10.5/fusefs/fuse_kludges.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2006-2007 Google. All Rights Reserved.
+ * Amit Singh <singh@>
+ */
+
+#ifndef _FUSE_KLUDGES_H_
+#define _FUSE_KLUDGES_H_
+
+#include "fuse.h"
+#include "fuse_sysctl.h"
+
+#include <sys/cdefs.h>
+#include <sys/mount.h>
+#include <sys/types.h>
+
+#endif /* _FUSE_KLUDGES_H_ */
